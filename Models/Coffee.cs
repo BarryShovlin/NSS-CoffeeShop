36b5346a36f974fa5a9831b1570db43fe153254e
Barry Shovlin <bshovlin32@gmail.com>
2021-04-08 15:41:40 -0500

    Bean varieties displayed

---
+++ Models/Coffee.cs
@@ -0,0 +1,12 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace CoffeeShop.Models
+{
+    public class Coffee
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public int BeanVarietyId { get; set; }
+        public BeanVariety BeanVariety { get; set; }
+    }
+}
